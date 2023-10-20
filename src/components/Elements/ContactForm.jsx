6a38a59c5d1358973f27f671b3c1b5ed52f48b5e
ailsoya <akalitata53@gmail.com>
2023-10-20 18:51:24 +0300

    Update ContactForm.jsx

--- src/components/Elements/ContactForm.jsx
+++ src/components/Elements/ContactForm.jsx
@@ -1,8 +1,45 @@
 import styles from "../Style.module.css"
-import { Component } from 'react'
+import { useState } from 'react'
 import { nanoid } from 'nanoid'
 
-class ContactForm extends Component {
+export const ContactForm = (props) => {
+    const [name, setName] = useState('')
+    const [number, setNumber] = useState('')
+
+    const handleChange = (evt) => {
+        const { name, value } = evt.target
+        if(name==='name') {
+            setName(value)
+        } else {
+            setNumber(value)
+        }
+    }
+
+    const prepareToSubmit = (evt) => {
+        const id = nanoid()
+        evt.preventDefault()
+        props.onSubmit({ id, name, number })
+        setName('')
+        setNumber('')
+
+    }
+
+    return (
+        <form onSubmit={evt => prepareToSubmit(evt)} className={styles.Form}>
+            <label className={styles.Label}>
+                Name
+                <input value={name} onChange={evt => handleChange(evt)} type="text" name="name" required />
+            </label>
+            <label className={styles.Label}>
+                Number
+                <input value={number} onChange={evt => handleChange(evt)} type="tel" name="number" required />
+            </label>
+            <button type='submit' className={styles.SubmitButton}>Add contact</button>
+        </form>
+    )
+}
+
+/*export class ContactForm extends Component {
     state = {
         name: '',
         number: ''
@@ -39,6 +76,4 @@ class ContactForm extends Component {
             </form>
         )
     }
-}
-
-export { ContactForm } 
+}*/
